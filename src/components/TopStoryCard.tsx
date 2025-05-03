e31bab7d1c69aadf5e556f7525f60028a437a5aa
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-03 16:55:57 +0530

    feat (TopStoryCard) : create UI with dummy data step 02

--- src/components/TopStoryCard.tsx
+++ src/components/TopStoryCard.tsx
@@ -1,24 +1,73 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 
 const TopStoryCard = () => {
   return (
     <Box
       sx={{
         border: "1px solid blue",
-        height: "500px",
+        height: "80dvh",
+        // height: "600px",
         mx: "25px",
-        p:"25px"
+        p: "25px",
+        bgcolor: "#1B1B1B",
       }}
     >
-      <Grid container spacing={"25px"}>
-        <Grid size={{ xs: 8 }}>
-          <Box sx={{ border: "1px solid green" }}>Sample text</Box>
+      <Grid
+        container
+        spacing={"25px"}
+        sx={{ border: "1px solid red", height: "100%" }}
+      >
+        {/* Text and info */}
+        <Grid
+          size={{ xs: 8 }}
+          sx={{
+            border: "1px solid white",
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+            height: "100%",
+          }}
+        >
+          {/* Title */}
+          <Typography
+            sx={{
+              color: "green",
+              fontSize: "64px",
+              lineHeight: "80px",
+              border: "1px solid blue",
+              flex: 3,
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              // height:"100%"
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </Typography>
+          {/* Subtitle */}
+          <Typography
+            sx={{
+              color: "green",
+              fontSize: "28px",
+              lineHeight: "40px",
+              border: "1px solid blue",
+              flex: 4,
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              // height:"100%"
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea laborum
+            minus dolores, tenetur cupiditate perspiciatis quasi magni ipsa
+            deleniti? Facilis nostrum illo quia fugiat perferendis numquam quos
+            debitis suscipit quae?. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Mollitia distinctio, cum atque aut vel labore a
+            ratione ea consectetur, eos eveniet? Quo quod ratione repudiandae
+            esse optio ea reiciendis ipsa.
+          </Typography>
+          <Box sx={{ flex: 1, border: "1px solid red" }}></Box>
         </Grid>
-        <Grid size={{ xs: 4 }}>
-          <Box
-            sx={{ border: "1px solid white" }}
-          >Sample text</Box>
-        </Grid>
+        {/* Image placeholder */}
+        <Grid size={{ xs: 4 }} sx={{ border: "1px solid white" }}></Grid>
       </Grid>
     </Box>
   );
