c996bc9a5ad9a9ce6c0514033944336e906282d8
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-04 22:22:44 +0530

    feat(api): implement newsService with fetchNewsById function

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -1,9 +1,26 @@
 import axios from "axios";
-import { apiRequest, NewsResponse } from "./newsClient";
+import { apiRequest, News, NewsResponse } from "./newsClient";
 
 export const fetchNews = async (): Promise<NewsResponse | null> => {
   try {
     return await apiRequest<NewsResponse>("/v4/articles/", "GET");
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.message);
+      console.error(error.code);
+      throw error; //rethrowing errorr
+    } else {
+      console.error(error);
+      throw error; //rethrowing errorr
+    }
+  }
+};
+
+export const fetchNewsById = async (
+  id: number
+): Promise<News | null> => {
+  try {
+    return await apiRequest<News>(`/v4/articles/${id}`, "GET");
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.error(error.message);
