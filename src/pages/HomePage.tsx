e0517ba8ddd1255313ff23152dff5c6827a67f2f
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-04 15:04:12 +0530

    feat(HomePage): add TopStoryCard and NewsSection components (with dummy data) to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import { Box } from "@mui/material";
 import TopStoryCard from "../components/TopStoryCard";
+import NewsSection from "../components/NewsSection";
 
 const HomePage = () => {
   return (
     <Box>
+      <Box sx={{ height: "100px" }}></Box>
       <TopStoryCard />
+      <NewsSection />
+      <Box sx={{ height: "500px" }}></Box>
     </Box>
   );
 };
