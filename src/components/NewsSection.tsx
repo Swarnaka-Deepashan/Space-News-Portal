b509eb709fd797a6c0c9a5cc375e488cdbcda74a
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-04 19:55:09 +0530

    feat(NewsSection): manage loading and error states while fetching news

--- src/components/NewsSection.tsx
+++ src/components/NewsSection.tsx
@@ -1,29 +1,89 @@
-import { Grid } from "@mui/material"
-import NewsCard from "./NewsCard"
+import { Box, CircularProgress, Grid, Typography } from "@mui/material";
+import NewsCard from "./NewsCard";
+import { useAppSelector } from "../app/hook";
 
 const NewsSection = () => {
-  return (
-    <Grid container spacing={"15px"} sx={{ mx: "25px", mt: "100px" }}>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <NewsCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <NewsCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <NewsCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <NewsCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <NewsCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <NewsCard />
-        </Grid>
-      </Grid>
-  )
-}
+  const news = useAppSelector((state) => state.news.newsData);
+  const newsLoadingState = useAppSelector(
+    (state) => state.news.newsLoadingState
+  );
+  const newsErrorState = useAppSelector((state) => state.news.newsErrorState);
 
-export default NewsSection
+  if (newsLoadingState) {
+    return (
+      <Box
+        sx={{
+          bgcolor: "#1B1B1B",
+          width: "auto",
+          height: "500px",
+          mx: "25px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CircularProgress size={60} />
+      </Box>
+    );
+  } else if (newsErrorState) {
+    return (
+      <Box
+        sx={{
+          bgcolor: "#1B1B1B",
+          width: "auto",
+          height: "500px",
+          mx: "25px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: "100px"
+        }}
+      >
+        <Typography
+          sx={{ color: "white", fontStyle: "italic", fontSize: "18px" ,
+
+          }}
+        >
+          Error Fetching News
+        </Typography>
+      </Box>
+    );
+  } else if (!news) {
+    return (
+      <Box
+        sx={{
+          bgcolor: "#1B1B1B",
+          width: "auto",
+          height: "500px",
+          mx: "25px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: "100px",
+        }}
+      >
+        <Typography
+          sx={{ color: "white", fontStyle: "italic", fontSize: "18px" }}
+        >
+          No Data to Display
+        </Typography>
+      </Box>
+    );
+  } else {
+    return (
+      <Grid container spacing={"15px"} sx={{ mx: "25px", mt: "100px" }}>
+        {news.map((newsObj) => (
+          <Grid size={{ xs: 12, md: 6 }}>
+            <NewsCard
+              title={newsObj.title}
+              summary={newsObj.summary}
+              imagePath={newsObj.image_url}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+};
+
+export default NewsSection;
