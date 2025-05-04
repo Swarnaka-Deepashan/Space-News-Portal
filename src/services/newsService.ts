454a8afd293e59f9faac269934bd767df7616046
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-04 18:56:40 +0530

    chore(deps): modified newsService to rethrow errors

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -1,10 +1,17 @@
+import axios from "axios";
 import { apiRequest, NewsResponse } from "./newsClient";
 
 export const fetchNews = async (): Promise<NewsResponse | null> => {
   try {
     return await apiRequest<NewsResponse>("/v4/articles/", "GET");
   } catch (error) {
-    console.error("Failed to fetch News:", error);
-    return null;
+    if (axios.isAxiosError(error)) {
+      console.error(error.message);
+      console.error(error.code);
+      throw error; //rethrowing errorr
+    } else {
+      console.error(error);
+      throw error; //rethrowing errorr
+    }
   }
 };
