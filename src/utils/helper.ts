26b3b50cb176a79ebbabc319f7ce1eb972013c03
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-05 05:28:55 +0530

    feat(helper): add generateTimeLabel utility function for generating dummy time label

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -17,3 +17,17 @@ export const formatPublishedDate = (isoDate: string) => {
     year: "numeric",
   });
 };
+
+export const generateTimeLabel = (isExtraSmallScreen:boolean) => {
+
+    //random digit between 1 and 5
+  const randomInt = Math.floor(Math.random() * 5) + 1;
+
+  if (randomInt === 1) {
+    return isExtraSmallScreen ? "1 min read" : "1 minute read" ;
+  } else {
+    return isExtraSmallScreen
+      ? `${randomInt} mins read`
+      : `${randomInt} minutes read`;
+  }
+};
