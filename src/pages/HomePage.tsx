fea4d32b2d8a8e20039244a73d68151a505e8aa4
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-04 17:45:40 +0530

    feat(homepage): fetch news data on mount with Redux state management for loading and errors

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,43 @@
 import { Box } from "@mui/material";
 import TopStoryCard from "../components/TopStoryCard";
 import NewsSection from "../components/NewsSection";
+import { useEffect } from "react";
+import { fetchNews } from "../services/newsService";
+
+// import { useAppSelector } from 'app/hooks'
+import { useAppSelector,useAppDispatch } from "../app/hook";
+import { setNews, setNewsErrorState, setNewsLoadingState } from "../app/slices/newsSlice";
+
 
 const HomePage = () => {
+  const news = useAppSelector((state) => state.news.newsData);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const fetchNewsData = async () => {
+      try {
+        dispatch(setNewsLoadingState(true));
+
+        const response = await fetchNews();
+
+        if (response && response.results.length > 0) {
+          dispatch(setNews(response.results));
+          dispatch(setNewsErrorState(false));
+        } else {
+          dispatch(setNewsErrorState(true));
+        }
+      } catch (err) {
+        dispatch(setNewsErrorState(true));
+        console.error(err, "Failed to fetch news.");
+      } finally {
+        dispatch(setNewsLoadingState(false));
+      }
+    };
+
+    fetchNewsData();
+  }, [dispatch]);
+
+  console.log("30 news", news);
   return (
     <Box>
       <Box sx={{ height: "100px" }}></Box>
