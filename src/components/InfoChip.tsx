e222d800bddf80a09560da1d250c3ed2e800b186
Swarnaka-Deepashan <swarnakadeepashan@gmail.com>
2025-05-04 09:51:49 +0530

    style(InfoChip): adjust InfoChip text fontsize

--- src/components/InfoChip.tsx
+++ src/components/InfoChip.tsx
@@ -18,8 +18,10 @@ const InfoChip: React.FC<InfoChipProps> = ({
   return (
     <Box
       sx={{
-        height: "40px",
-        width: "160px",
+        height: "30px",
+        // width: "160px",
+        maxWidth: "160px",
+        width: "fit-content",
         borderRadius: "50px",
         background: `linear-gradient(45deg, ${gradientStartColor} 30%, ${gradientEndColor} 90%)`,
         display: "flex",
@@ -33,15 +35,17 @@ const InfoChip: React.FC<InfoChipProps> = ({
       <IconComponent sx={{ flex: 1, color: "white" }} />
       <Typography
         sx={{
-          color: "white",
-          fontSize: "20px",
+          // color: "white",
+          color: "#E0E0E0",
+          fontSize: "16px",
+          // fontSize: { xs: "12px", sm: "22px", md: "28px" },
           maxWidth: "100%",
           overflow: "hidden",
           textWrap: "nowrap",
           textOverflow: "ellipsis",
           flex: 5,
           fontStyle: "italic",
-          fontWeight:"300"
+          fontWeight: "300",
         }}
       >
         {text}
